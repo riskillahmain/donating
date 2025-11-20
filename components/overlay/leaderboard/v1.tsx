26e537a3468be90411349c5e917326678616f09d
PRINCE NOVAN <princenovan@MacBook-Air-PRINCE.local>
2025-11-20 19:24:14 +0700

    bug2

--- components/overlay/leaderboard/v1.tsx
+++ components/overlay/leaderboard/v1.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence, type Variants } from "framer-motion";
 
 type ThemeOption = 'pink' | 'blue' | 'green' | 'purple';
 
